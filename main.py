e946f6280b63f6413d1b4625b01aea06e31013be
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-02-08 20:49:33 +0800

    Visualising dendogram method

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3,4]].values
 
+# Find ideal number of cluster with dendogram method
 import scipy.cluster.hierarchy as sch
-
 dendogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title("Dendogram")
+plt.xlabel("Customers")
+plt.ylabel("Euclidean distance")
+plt.show()
